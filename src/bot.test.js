1670f257463109ac3e209918e1c9e8deba03c7aa
Brian Moseley <bcm@maz.org>
2021-09-25 06:47:41 -0400

    ping/pong message event
    just want to demonstrate listening for a message and replying to it

--- src/bot.test.js
+++ src/bot.test.js
@@ -1,15 +1,39 @@
 const Bot = require("./bot");
 
-const token = "deadbeef";
+const TOKEN = "deadbeef";
+
+let bot;
+beforeEach(() => (bot = new Bot(TOKEN)));
+
+describe("onMessage", () => {
+  test("pongs when pinged", () => {
+    const msg = {
+      content: "ping",
+      reply: jest.fn(),
+    };
+    bot.onMessageCreate(msg);
+
+    expect(msg.reply).toHaveBeenCalledWith("pong");
+  });
+
+  test("doesn't pong otherwise", () => {
+    const msg = {
+      content: "hi",
+      reply: jest.fn(),
+    };
+    bot.onMessageCreate(msg);
+
+    expect(msg.reply).not.toHaveBeenCalled();
+  });
+});
 
 describe("start", () => {
   test("logs in", () => {
-    const bot = new Bot(token);
     // mock out login to suppress its side effects
     const spy = jest.spyOn(bot.client, "login").mockImplementation(() => {});
 
     bot.start();
 
-    expect(spy).toHaveBeenCalledWith(token);
+    expect(spy).toHaveBeenCalledWith(TOKEN);
   });
 });
