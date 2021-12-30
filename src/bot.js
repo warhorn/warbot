c4d1153fd26f845a4c1e57f4fdfdce23369df876
Brian Moseley <bcm@maz.org>
2021-12-30 09:54:03 -0500

    bot can receive DMs now

--- src/bot.js
+++ src/bot.js
@@ -2,10 +2,34 @@ const { Client, Intents } = require("discord.js");
 
 class Bot {
   constructor(token) {
-    this.client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+    this.client = new Client({
+      intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+      ],
+      // https://github.com/discordjs/discord.js/issues/5516
+      partials: ["CHANNEL"],
+    });
     this.token = token;
 
-    this.client.once("ready", () => console.log("Bot is ready for service"));
+    // this.client.on("debug", console.log);
+
+    this.client.once("ready", () => {
+      console.log(
+        `Bot is logged in as '${this.client.user.tag}' && ready for service`
+      );
+    });
+
+    this.client.on("messageCreate", (message) => {
+      console.log(
+        `messageCreate: ${message.author.tag} said '${message.content}'`
+      );
+    });
   }
 
   start() {
